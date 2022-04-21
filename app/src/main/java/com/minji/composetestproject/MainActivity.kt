742232b66ad2002d5cefeca0a310ccc31112255d
정민지 <minji@mobmail.shinhan.com>
2022-04-21 14:35:40 +0900

    Tutorial Step4 - Lists and animations
    - Create a list of messages
    - Animate message while expanding

--- app/src/main/java/com/minji/composetestproject/MainActivity.kt
+++ app/src/main/java/com/minji/composetestproject/MainActivity.kt
@@ -4,9 +4,13 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -16,6 +20,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.minji.composetestproject.ui.theme.ComposeTestProjectTheme
 
@@ -24,13 +34,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTestProjectTheme {
-                MessageCard(Message("Android", "Jetpack Compose"))
+                Conversation(messages = SampleData.conversationSample)
             }
         }
     }
 }
 
 data class Message(val author: String, val body: String)
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    ComposeTestProjectTheme {
+        Conversation(messages = SampleData.conversationSample)
+    }
+}
 
 @Composable
 fun MessageCard(msg: Message) {
@@ -46,18 +73,34 @@ fun MessageCard(msg: Message) {
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary
+            else MaterialTheme.colors.surface
+        )
+
+        Column(
+            modifier = Modifier.clickable { isExpanded = !isExpanded }
+        ) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle2
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     style = MaterialTheme.typography.body2,
-                    modifier = Modifier.padding(all = 4.dp)
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                 )
             }
         }
@@ -70,6 +113,7 @@ fun MessageCard(msg: Message) {
     showBackground = true,
     name = "Dark Mode"
 )
+
 @Composable
 fun PreviewMessageCard() {
     ComposeTestProjectTheme {
